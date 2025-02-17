235e2f2b43cc0fa7dca9e8cce34f6ac13d1e91b5
Ajay More <more.ajay6995@gmail.com>
2025-02-17 20:01:18 +0530

    skills and about page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  public title = 'portfolio';
+
+  ngOnInit() {
+    const options = {
+      strings: ["Software Developer", "Frontend Developer", "JavaScript Developer", "MEAN Stack Developer", "MERN Stack Developer", "Playwright Developer"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 500,
+      loop: true,
+    };
+
+    new Typed("#typed-output", options);
+  }
 }
