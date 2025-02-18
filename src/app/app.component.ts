1e4e510b8c9dde46876e93643a0a2f4009617ecf
Ajay More <more.ajay6995@gmail.com>
2025-02-18 21:39:39 +0530

    Added Email service, Professional journey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Typed from 'typed.js';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,17 @@ import Typed from 'typed.js';
 })
 export class AppComponent implements OnInit {
   public title = 'portfolio';
+  public activeLink = 'home';
+  public isLoading = false;
+  @ViewChildren('sections') sections!: QueryList<any>;
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
     const options = {
@@ -20,5 +32,69 @@ export class AppComponent implements OnInit {
     };
 
     new Typed("#typed-output", options);
+
+    const accordionButtons = document.querySelectorAll('.accordion-button');
+    accordionButtons.forEach((button: Element) => {
+      (button as HTMLElement).addEventListener('click', function () {
+        const targetId = (this as HTMLElement).getAttribute('aria-controls');
+        if (targetId) {
+          const target = document.getElementById(targetId);
+          if (target) {
+            (this as HTMLElement).blur();
+            const headerOffset = 160; // Adjust this value based on your header height
+            const elementPosition = target.getBoundingClientRect().top + window.scrollY;
+            const offsetPosition = elementPosition - headerOffset;
+
+            window.scrollTo({
+              top: offsetPosition,
+              behavior: 'smooth'
+            });
+          }
+        }
+      });
+    });
+  }
+
+  ngAfterViewInit(): void {
+    window.onscroll = () => {
+      let currentSection = '';
+      const scrollPosition = window.pageYOffset + window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      if (scrollPosition >= documentHeight) {
+        this.activeLink = 'contact';
+      } else {
+        this.sections.forEach((section) => {
+          const sectionTop = section.nativeElement.offsetTop;
+          const sectionHeight = section.nativeElement.offsetHeight;
+          if (window.pageYOffset >= sectionTop - sectionHeight / 2) {
+            currentSection = section.nativeElement.getAttribute('id');
+          }
+        });
+
+        this.activeLink = currentSection;
+      };
+    };
+  }
+
+  setActiveLink(linkActive: string) {
+    console.log(linkActive)
+    this.activeLink = linkActive;
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.isLoading = true;
+      emailjs.send('more.ajay6995', 'template_zip9bs8', this.contactForm.value, 'uY4gXyME1PD6k7mPC')
+        .then((response: any) => {
+          this.isLoading = false;
+          alert('Message sent successfully!');
+          this.contactForm.reset();
+        })
+        .catch((error: any) => {
+          this.isLoading = false;
+          console.error('Error sending email:', error);
+          alert('Failed to send message. Please try again later.');
+        });
+    }
   }
 }
